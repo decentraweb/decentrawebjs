423a2f8b503da798e93e4171543971f7ca959f4f
Mauvis Ledford <switchstatement@gmail.com>
2022-05-06 19:57:14 +0000

    smart contract updates

--- packages/core/src/utils/contracts.ts
+++ packages/core/src/utils/contracts.ts
@@ -2,23 +2,28 @@ import {ethers, providers} from "ethers";
 import registryContract from "../contracts/DWEBRegistry.json";
 import publicResolverContract from "../contracts/PublicResolver.json";
 
-
 export const EthNetworkIds = {
-  'rinkeby': 4,
+  'mainnet': 1,
+  'rinkeby': 4
 }
 
-export type EthNetwork = 'rinkeby'
+export type EthNetwork = 'mainnet'
 
 export function getRegistryAddress(networkId: EthNetwork) {
-  if (EthNetworkIds[networkId]) {
+  if (networkId === 'rinkeby') {
     return '0xFF01dD9876244b6D4835Deb8441f9605bFBdBfb3'
+  }else if (networkId === 'mainnet') {
+    return '0x8eb93AB94A6Afa8d416aB1884Ebb5A3f00920a7A'
   }
   throw new Error('Unknown network ID')
 }
 export function getResolverAddress(networkId: EthNetwork) {
-  if (EthNetworkIds[networkId]) {
+  if (networkId === 'rinkeby') {
     return '0x4d8592961e2C68A0ce092D370EA2330B89091302'
+  }else if (networkId === 'mainnet') {
+    return '0xf157D3559DF1F8c69cb757A1A2cdF8736618E083'
   }
+
   throw new Error('Unknown network ID')
 }
 
