0c5f9f30d08c5f16e94dc03e25802943544207fb
IC <indraraj9999@gmail.com>
2022-05-07 10:42:30 +0530

    rinkeby network id corrected

--- packages/core/src/utils/contracts.ts
+++ packages/core/src/utils/contracts.ts
@@ -14,7 +14,7 @@ export function getRegistryAddress(networkId: EthNetwork) {
     case 1:
       return '0x8eb93AB94A6Afa8d416aB1884Ebb5A3f00920a7A'
       break;
-    case 2:
+    case 4:
       return '0xFF01dD9876244b6D4835Deb8441f9605bFBdBfb3'
       break;
     default:
@@ -26,7 +26,7 @@ export function getResolverAddress(networkId: EthNetwork) {
     case 1:
       return '0xf157D3559DF1F8c69cb757A1A2cdF8736618E083'
       break;
-    case 2:
+    case 4:
       return '0x4d8592961e2C68A0ce092D370EA2330B89091302'
       break;
     default:
