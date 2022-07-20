561dfdb7a9532370b166523606365247ff99b308
Sergiy Lytvynenko <lytvynenkosv@gmail.com>
2022-07-20 11:43:14 +0300

    Add forward resolution check for reverse wallet resolution (#4)

--- packages/core/src/utils/dns.ts
+++ packages/core/src/utils/dns.ts
@@ -1,5 +1,6 @@
 import {name} from "dns-packet";
 import {ethers} from "ethers";
+import {hash as namehash} from "@ensdomains/eth-ens-namehash";
 
 export function dnsWireName(domain: string): Buffer {
   return name.encode(domain)
@@ -7,4 +8,14 @@ export function dnsWireName(domain: string): Buffer {
 
 export function dnsWireNameHash(domain: string): string {
   return ethers.utils.keccak256(dnsWireName(domain))
+}
+
+export function isValidDomain(domain: string): boolean {
+  let isValid = true;
+  try {
+    namehash(domain);
+  } catch (e) {
+    isValid = false;
+  }
+  return isValid;
 }
