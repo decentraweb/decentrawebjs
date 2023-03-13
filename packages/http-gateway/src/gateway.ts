b925e52424bbe22dd7ed9c372b67f8b2f6b39a2f
Serhii Lytvynenko <lytvynenkosv@gmail.com>
2023-03-13 12:45:01 +0200

    Crash gateway on WebsocketProvider timeout

--- packages/http-gateway/src/gateway.ts
+++ packages/http-gateway/src/gateway.ts
@@ -4,6 +4,9 @@ import HTTPGateway from './index';
 import { providers } from 'ethers';
 import config from './config';
 
+const KEEPALIVE_INTERVAL = 10000;
+const PROVIDER_TIMEOUT = 3000;
+
 if (config.sentry_dsn) {
   Sentry.init({
     environment: process.env.NODE_ENV,
@@ -15,13 +18,23 @@ if (config.sentry_dsn) {
     tracesSampleRate: 0.1,
     integrations: [
       // enable HTTP calls tracing
-      new Sentry.Integrations.Http({ tracing: true }),
-    ],
+      new Sentry.Integrations.Http({ tracing: true })
+    ]
   });
   addExtensionMethods();
 }
 
 const provider = new providers.WebSocketProvider(config.websocket_url, config.eth_network);
+
+setInterval(() => {
+  const responseTimeout = setTimeout(() => {
+    throw new Error('Ethereum provider timed out');
+  }, PROVIDER_TIMEOUT);
+  provider.getBlockNumber().then(() => {
+    clearTimeout(responseTimeout);
+  });
+}, KEEPALIVE_INTERVAL);
+
 const gateway = new HTTPGateway({
   baseDomain: config.gateway_domain,
   ipfsGatewayIp: config.ipfs_gateway,
