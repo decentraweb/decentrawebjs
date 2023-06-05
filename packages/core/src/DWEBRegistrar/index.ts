5f0413423813d68f8ad40e0f304c420be500b2d8
Serhii Lytvynenko <lytvynenkosv@gmail.com>
2023-06-05 16:36:48 +0300

    Progress on registration support

--- packages/core/src/DWEBRegistrar/index.ts
+++ packages/core/src/DWEBRegistrar/index.ts
@@ -97,12 +97,6 @@ export class DWEBRegistrar extends DwebContractWrapper {
     };
   }
 
-  /**
-   * Mint TLDs
-   * @param request - approved registration request received from `requestApproval` method
-   * @param isFeesInDweb - if true, fees will be paid in DWEB, otherwise in ETH
-   * @param progressCb - callback to track the progress of the minting process
-   */
   @requiresSigner
   async register(
     request: CommittedRegistration,
@@ -144,13 +138,6 @@ export class DWEBRegistrar extends DwebContractWrapper {
     return registerTx.wait(1);
   }
 
-  /**
-   * Check registration fees against user balance (DWEB and ETH) and return amount of ETH to be sent with the transaction
-   * @param requests - array of TLDs to register
-   * @param isFeesInDweb - if true, fees will be paid in DWEB, otherwise in ETH
-   * @param signerAddress - address of the account that will pay the fees
-   * @private
-   */
   async verifySignerBalance(
     request: RegistrationContext,
     isFeesInDweb: boolean,
